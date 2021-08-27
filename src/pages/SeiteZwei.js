9322c7fc55c2357c5e7bc416a8e4e83562e4f3b9
gerold <gerold.ibach@googlemail.com>
2021-08-27 19:40:44 +0200

    tutorial part 2

---
+++ src/pages/SeiteZwei.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const SeiteZwei = () => {
+  return (
+    <Layout pageTitle="SeiteZwei">
+      <p>Seite zwei Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+}
+
+export default SeiteZwei
